d5225b941463daae2accd7508bee8ec8a63700c5
SonMooSans <god63820869@gmail.com>
2022-08-10 22:45:35 +0800

    Supports verify permissions

--- src/main/kotlin/com/bdash/api/utils/Helper.kt
+++ src/main/kotlin/com/bdash/api/utils/Helper.kt
@@ -1,11 +1,13 @@
 package com.bdash.api.utils
 
 import com.bdash.api.UserSession
+import com.bdash.api.discord.DiscordApi
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.sessions.*
 import io.ktor.util.pipeline.*
+import net.dv8tion.jda.api.Permission
 
 suspend inline fun PipelineContext<Unit, ApplicationCall>.withSession(receiver: (UserSession) -> Unit) {
     val session = call.sessions.get<UserSession>()
@@ -14,5 +16,21 @@ suspend inline fun PipelineContext<Unit, ApplicationCall>.withSession(receiver: 
         receiver(session)
     } else {
         call.respond(HttpStatusCode.Unauthorized, "Not Authorized")
+    }
+}
+
+suspend inline fun PipelineContext<Unit, ApplicationCall>.verify(guildId: String, onSuccess: () -> Unit) = withSession {
+    val guild = DiscordApi.getGuild(it, guildId) ?: return@withSession
+
+    val permissions = Permission.getPermissions(
+        guild.permissions!!.toLong()
+    )
+
+    val isAdmin = permissions.contains(Permission.ADMINISTRATOR)
+
+    if (isAdmin) {
+        onSuccess()
+    } else {
+        call.respond(HttpStatusCode.Unauthorized, "Missing Required Permission")
     }
 }
